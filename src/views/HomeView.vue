1b508a247aaa9fe5cbd8d16ea4644f81663a23cd
Diana Martinez <diananerdoficial@gmail.com>
2022-04-28 17:40:07 -0500

    Navegacion programatica

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,11 @@ export default {
     return {
       username: ''
     }
+  },
+  methods: {
+    iniciar() {
+      this.$router.go(-1)
+    }
   }
 }
 </script>
@@ -12,5 +17,6 @@ export default {
   <main>
     <h3>Introduce tu nombre de usuario</h3>
     <input placeholder="username" v-model="username" />
+    <button @click="iniciar">Iniciar sesion</button>
   </main>
 </template>
