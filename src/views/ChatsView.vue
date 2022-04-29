57849074b570c0d6e0edb1c0a7f03a4adfcc2fa7
Diana Martinez <diananerdoficial@gmail.com>
2022-04-29 18:44:37 -0500

    Uso de useRoute, useRouter y Composition API

--- src/views/ChatsView.vue
+++ src/views/ChatsView.vue
@@ -13,26 +13,23 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      chats: []
-    }
-  },
-  created() {
-    this.$watch(
-      () => this.$route.params,
-      (val) => {
-        console.log('update params', val)
-        this.chats = [
-          { id: 1, name: 'Ximena' },
-          { id: 2, name: 'Daniel' },
-          { id: 3, name: 'Miguel' }
-        ]
-      },
-      { immediate: true }
-    )
-  },
-}
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+const chats = ref()
+
+const route = useRoute()
+
+watch(
+  () => route.params,
+  (val) => {
+  console.log('update params', val)
+  chats.value = [
+    { id: 1, name: 'Ximena' },
+    { id: 2, name: 'Daniel' },
+    { id: 3, name: 'Miguel' }
+  ]
+}, {
+  immediate: true
+})
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,12 @@
-<script>
-export default {
-  data() {
-    return {
-      username: ''
-    }
-  },
-  methods: {
-    iniciar() {
-      this.$router.push({ name: 'about' })
-    }
-  }
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const username = ref()
+
+const router = useRouter()
+
+function iniciar() {
+  router.push({ name: 'about' })
 }
 </script>
 
